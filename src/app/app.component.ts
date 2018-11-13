721581c6619669d2f443dfae2464d4350885d16f
ali_mousavi <alimusavi67@gmail.com>
2018-11-13 12:42:24 +0330

    simple prof for multi language test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from './translate.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+
+  constructor(private translate: TranslateService) {
+    console.log(translate.data);
+  }
+
+  setLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
